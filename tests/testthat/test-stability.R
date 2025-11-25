ef0eebeacc76dc34db520fe94965e0d07e323f9a
Akira Terui <hanabi0111@gmail.com>
2025-11-25 16:57:08 -0500

    📝 update test function

--- tests/testthat/test-stability.R
+++ tests/testthat/test-stability.R
@@ -1,12 +1,9 @@
 
-# setup -------------------------------------------------------------------
+# two species case --------------------------------------------------------
 
 x0 <- runif(2, 0, 1)
 A <- rbind(c(runif(1, -1, 0), 0),
            c(runif(1, -1, 0), runif(1, -1, 0)))
-
-
-# test --------------------------------------------------------------------
 
 test_that("stability, two-species system GLV", {
 
@@ -95,3 +92,87 @@ test_that("stability, two-species system Beverton-Holt", {
 
   expect_equal(lm_max0, lm_max1, tolerance = 1e-6)
 })
+
+
+# more than 2 species -----------------------------------------------------
+
+set.seed(123)
+n_species_list <- c(3, 4)
+
+for (n_species in n_species_list) {
+
+  x0 <- runif(n_species, 0.1, 1)
+  A <- matrix(runif(n_species^2, -1, 0), n_species, n_species)
+
+  test_that(paste("stability,", n_species, "species system GLV"), {
+
+    r <- drop(-(A %*% x0))
+
+    # Jacobian
+    J <- t(sapply(seq_len(n_species), function(i) {
+      fun_partial(r = r[i],
+                  a = A[i, ],
+                  x0 = x0,
+                  i = i,
+                  model = "glv")
+    }))
+
+    lm_max0 <- max(eigen(J)$values)
+    lm_max1 <- stability(n_species = n_species,
+                         r = r,
+                         x0 = x0,
+                         alpha = A,
+                         model = "glv")
+    lm_max1 <- c(lm_max1) # remove attributes
+
+    expect_equal(lm_max0, lm_max1, tolerance = 1e-6)
+  })
+
+  test_that(paste("stability,", n_species, "species system Ricker"), {
+
+    r <- drop(-(A %*% x0))
+
+    J <- t(sapply(seq_len(n_species), function(i) {
+      fun_partial(r = r[i],
+                  a = A[i, ],
+                  x0 = x0,
+                  i = i,
+                  model = "ricker")
+    }))
+
+    lm_max0 <- max(eigen(J)$values)
+    lm_max1 <- stability(n_species = n_species,
+                         r = r,
+                         x0 = x0,
+                         alpha = A,
+                         model = "ricker")
+    lm_max1 <- c(lm_max1)
+
+    expect_equal(lm_max0, lm_max1, tolerance = 1e-6)
+  })
+
+  test_that(paste("stability,", n_species, "species system Beverton-Holt"), {
+
+    A0 <- abs(A)
+    lambda_vec <- 1 + A0 %*% x0
+    r <- log(lambda_vec)
+
+    J <- t(sapply(seq_len(n_species), function(i) {
+      fun_partial(r = r[i],
+                  a = A0[i, ],
+                  x0 = x0,
+                  i = i,
+                  model = "bh")
+    }))
+
+    lm_max0 <- max(eigen(J)$values)
+    lm_max1 <- stability(n_species = n_species,
+                         r = r,
+                         x0 = x0,
+                         alpha = A0,
+                         model = "bh")
+    lm_max1 <- c(lm_max1)
+
+    expect_equal(lm_max0, lm_max1, tolerance = 1e-6)
+  })
+}
